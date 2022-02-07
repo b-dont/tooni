9e0dcd322e46b926df06d6cdbd7372e39bdf6f57
Brandon Phillips <brandont.dev@protonmail.com>
2022-02-07 12:58:26 -0700

    Adjusting Background tables in ORM

--- src/data/tables.rs
+++ src/data/tables.rs
@@ -18,11 +18,11 @@ pub enum Table {
 impl Table {
     pub fn name(&self) -> String {
         match self {
-            &Table::ProficiencyTable        => "proficiencies".to_string(),
-            &Table::LanguagesTable          => "languages".to_string(),
-            &Table::ItemsTable              => "items".to_string(),
-            &Table::FeaturesTable           => "features".to_string(),
-            &Table::BackgroundsTable        => "backgrounds".to_string(),
+            &Table::ProficiencyTable => "proficiencies".to_string(),
+            &Table::LanguagesTable   => "languages".to_string(),
+            &Table::ItemsTable       => "items".to_string(),
+            &Table::FeaturesTable    => "features".to_string(),
+            &Table::BackgroundsTable => "backgrounds".to_string(),
         }
     }
 
@@ -61,7 +61,33 @@ impl Table {
             .to_string(),
             &Table::BackgroundsTable => "
                 id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL
+                name TEXT NOT NULL,
+                personality_trait_1 TEXT NOT NULL,
+                personality_trait_2 TEXT NOT NULL,
+                personality_trait_3 TEXT NOT NULL,
+                personality_trait_4 TEXT NOT NULL,
+                personality_trait_5 TEXT NOT NULL,
+                personality_trait_6 TEXT NOT NULL,
+                personality_trait_7 TEXT NOT NULL,
+                personality_trait_8 TEXT NOT NULL,
+                ideal_1 TEXT NOT NULL,
+                ideal_2 TEXT NOT NULL,
+                ideal_3 TEXT NOT NULL,
+                ideal_4 TEXT NOT NULL,
+                ideal_5 TEXT NOT NULL,
+                ideal_6 TEXT NOT NULL,
+                bond_1 TEXT NOT NULL,
+                bond_2 TEXT NOT NULL,
+                bond_3 TEXT NOT NULL,
+                bond_4 TEXT NOT NULL,
+                bond_5 TEXT NOT NULL,
+                bond_6 TEXT NOT NULL,
+                flaw_1 TEXT NOT NULL,
+                flaw_2 TEXT NOT NULL,
+                flaw_3 TEXT NOT NULL,
+                flaw_4 TEXT NOT NULL,
+                flaw_5 TEXT NOT NULL,
+                flaw_6 TEXT NOT NULL
                 "
             .to_string(),
         }
@@ -69,21 +95,81 @@ impl Table {
 
     pub fn queries(&self) -> String {
         match self {
-            &Table::ProficiencyTable        => "id, name, class".to_string(),
-            &Table::LanguagesTable          => "id, name, description".to_string(),
-            &Table::ItemsTable              => "id, name, class, quantity, rarity, value, weight, properties, description".to_string(),
-            &Table::FeaturesTable           => "id, class, name, description".to_string(),
-            &Table::BackgroundsTable        => "id, name".to_string(),
+            &Table::ProficiencyTable => "id, name, class".to_string(),
+            &Table::LanguagesTable   => "id, name, description".to_string(),
+            &Table::ItemsTable       => "id, name, class, quantity, rarity, value, weight, properties, description".to_string(),
+            &Table::FeaturesTable    => "id, class, name, description".to_string(),
+            &Table::BackgroundsTable => {"
+                id, 
+                name,
+                personality_trait_1 TEXT NOT NULL,
+                personality_trait_2 TEXT NOT NULL,
+                personality_trait_3 TEXT NOT NULL,
+                personality_trait_4 TEXT NOT NULL,
+                personality_trait_5 TEXT NOT NULL,
+                personality_trait_6 TEXT NOT NULL,
+                personality_trait_7 TEXT NOT NULL,
+                personality_trait_8 TEXT NOT NULL,
+                ideal_1 TEXT NOT NULL,
+                ideal_2 TEXT NOT NULL,
+                ideal_3 TEXT NOT NULL,
+                ideal_4 TEXT NOT NULL,
+                ideal_5 TEXT NOT NULL,
+                ideal_6 TEXT NOT NULL,
+                bond_1 TEXT NOT NULL,
+                bond_2 TEXT NOT NULL,
+                bond_3 TEXT NOT NULL,
+                bond_4 TEXT NOT NULL,
+                bond_5 TEXT NOT NULL,
+                bond_6 TEXT NOT NULL,
+                flaw_1 TEXT NOT NULL,
+                flaw_2 TEXT NOT NULL,
+                flaw_3 TEXT NOT NULL,
+                flaw_4 TEXT NOT NULL,
+                flaw_5 TEXT NOT NULL,
+                flaw_6 TEXT NOT NULL
+                ".to_string()
+            },
         }
     }
 
     pub fn values(&self) -> String {
         match self {
-            &Table::ProficiencyTable        => "?1, ?2, ?3".to_string(),
-            &Table::LanguagesTable          => "?1, ?2, ?3".to_string(),
-            &Table::ItemsTable              => "?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9".to_string(),
-            &Table::FeaturesTable           => "?1, ?2, ?3, ?4".to_string(),
-            &Table::BackgroundsTable        => "?1, ?2".to_string(),
+            &Table::ProficiencyTable => "?1, ?2, ?3".to_string(),
+            &Table::LanguagesTable   => "?1, ?2, ?3".to_string(),
+            &Table::ItemsTable       => "?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9".to_string(),
+            &Table::FeaturesTable    => "?1, ?2, ?3, ?4".to_string(),
+            &Table::BackgroundsTable => {"
+                ?1, 
+                ?2,
+                ?3,
+                ?4,
+                ?5,
+                ?6,
+                ?7,
+                ?8,
+                ?9,
+                ?10,
+                ?11,
+                ?12,
+                ?13,
+                ?14,
+                ?15,
+                ?16,
+                ?17,
+                ?18,
+                ?19,
+                ?20,
+                ?21,
+                ?22,
+                ?23,
+                ?24,
+                ?25,
+                ?26,
+                ?27,
+                ?28
+                ".to_string()
+            },
         }
     }
 
@@ -123,10 +209,40 @@ impl Table {
                 languages: Vec::new(),
                 starting_equipment: Vec::new(),
                 features: Vec::new(),
-                personality_traits: Vec::new(),
-                ideals: Vec::new(),
-                bonds: Vec::new(),
-                flaws: Vec::new(),
+                personality_traits: vec![
+                    row.get(2)?,
+                    row.get(3)?,
+                    row.get(4)?,
+                    row.get(5)?,
+                    row.get(6)?,
+                    row.get(7)?,
+                    row.get(8)?,
+                    row.get(9)?,
+                ],
+                ideals: vec![
+                    row.get(10)?,
+                    row.get(11)?,
+                    row.get(12)?,
+                    row.get(13)?,
+                    row.get(14)?,
+                    row.get(15)?,
+                ],
+                bonds: vec![
+                    row.get(16)?,
+                    row.get(17)?,
+                    row.get(18)?,
+                    row.get(19)?,
+                    row.get(20)?,
+                    row.get(21)?,
+                ],
+                flaws: vec![
+                    row.get(22)?,
+                    row.get(23)?,
+                    row.get(24)?,
+                    row.get(25)?,
+                    row.get(26)?,
+                    row.get(27)?,
+                ],
             })),
         }
     }
